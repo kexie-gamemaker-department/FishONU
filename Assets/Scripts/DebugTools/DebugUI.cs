34712ffd5f27babcf8277c7848c9acd4f55b9484
FishCat233 <50800434+FishCat233@users.noreply.github.com>
2026-02-07 00:12:16 +0800

    :construction: dev: 添加了 TextMeshPro 库，并且写了个简单的 DebugMenu

---
+++ Assets/Scripts/DebugTools/DebugUI.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using UnityEngine;
+
+namespace FishONU.DebugTools
+{
+    public class DebugUI : MonoBehaviour
+    {
+        public bool Display = false;
+
+        public string address { get; private set; }
+        public string port { get; private set; }
+
+        public void Awake()
+        {
+            address = "127.0.0.1";
+            port = "7777";
+        }
+
+        private void OnGUI()
+        {
+            if (Display) DrawMenu();
+        }
+
+        private void DrawMenu()
+        {
+            GUI.Box(new Rect(20, 20, 400, 400), "Debug Menu");
+            GUILayout.BeginArea(new Rect(25, 45, 350, 300));
+            GUILayout.BeginVertical("box");
+            GUILayout.BeginHorizontal();
+            GUILayout.Label("Address: ");
+            address = GUILayout.TextField(address);
+            GUILayout.EndHorizontal();
+            GUILayout.BeginHorizontal();
+            GUILayout.Label("Port: ");
+            port = GUILayout.TextField(port);
+            GUILayout.EndHorizontal();
+            if (GUILayout.Button("Connect", GUILayout.Width(100)))
+            {
+                Debug.Log($"Try to connect {address}:{port}");
+            }
+
+            GUILayout.EndVertical();
+            GUILayout.EndArea();
+        }
+    }
+}
