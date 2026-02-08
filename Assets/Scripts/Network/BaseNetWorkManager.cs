02769e11e04220f85b408ee7717d941d7620098d
FishCat233 <50800434+FishCat233@users.noreply.github.com>
2026-02-08 21:38:13 +0800

    dev: 给 BaseNetWorkManager 加入 PlayerCount 属性

--- Assets/Scripts/Network/BaseNetWorkManager.cs
+++ Assets/Scripts/Network/BaseNetWorkManager.cs
@@ -1,9 +1,12 @@
-﻿using Mirror;
+﻿using FishONU.Player;
+using Mirror;
+using UnityEngine;
 
 
 namespace FishONU.Network
 {
     public class BaseNetWorkManager : NetworkManager
     {
+        public int PlayerCount => GameObject.FindGameObjectsWithTag("Player").Length;
     }
 }
