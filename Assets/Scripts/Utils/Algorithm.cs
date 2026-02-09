912fbf010baffe124f450e3b6d692b1958b98461
FishCat233 <50800434+FishCat233@users.noreply.github.com>
2026-02-09 15:24:42 +0800

    feat(Utils): 实现了 Algorithm 扩展了 IList 的方法
    - 扩展了 void InsertRandom<T>(this IList<T> list, T item)
    - 扩展了 bool TryPop<T>(this IList<T> list, out T item)
    - 扩展了 void FisherYatesShuffle<T>(this IList<T> list)

---
+++ Assets/Scripts/Utils/Algorithm.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Random = UnityEngine.Random;
+
+namespace FishONU.Utils
+{
+    public static class Algorithm
+    {
+        public static void FisherYatesShuffle<T>(this IList<T> list)
+        {
+            for (int i = 0; i < list.Count; i++)
+            {
+                var j = Random.Range(0, i + 1);
+                (list[j], list[i]) = (list[i], list[j]);
+            }
+        }
+
+        public static bool TryPop<T>(this IList<T> list, out T item)
+        {
+            if (list == null) throw new ArgumentNullException(nameof(list));
+
+            if (list.Count == 0)
+            {
+                item = default(T);
+                return false;
+            }
+
+            var index = list.Count - 1;
+            item = list[index];
+            list.RemoveAt(index);
+
+            return true;
+        }
+
+        public static void InsertRandom<T>(this IList<T> list, T item)
+        {
+            int index = Random.Range(0, list.Count + 1);
+            list.Insert(index, item);
+        }
+    }
+}
